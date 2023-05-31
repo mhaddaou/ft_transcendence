6816fa11ac4faf5df5784c5fd81d246938d3531d
Mohamed Haddaoui <mhaddaou@e2r13p2.1337.ma>
2023-05-31 17:40:44 +0100

    update page chat and add contact list inside it

--- front-end/components/FormName.tsx
+++ front-end/components/FormName.tsx
@@ -1,19 +1,59 @@
 
+import { useRef, useState } from "react";
+
+function containsSpecialChars(str : string) : boolean {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  }
+
+
+function checkInputName(name : string) : void{
+    if (!containsSpecialChars(name) && name.length > 7){
+        // data ready to send it to the server
+        console.log("send data");
+    }
+    else{
+        // data is malicious code
+        console.warn(`name is ${name}`);
+    }
+}
+
 
 const FormName = () =>{
-    const name = "mhaddaou"
+    const value  = useRef<HTMLInputElement| null>(null);
+    const [valueName , setValue ] = useState<string>(''); 
+    var name = "mhaddaou";
+    const handl = ( event : any) =>{
+        if (event.key === "Enter"){
+            if (value.current){
+                checkInputName(value.current.value);
+            }
+        }
+
+    }
+
+
+    const submit = () =>{
+        if (value.current){
+            checkInputName(value.current.value);
+        }
+
+    }
+
+
+
     return (
-        <form className="w-full max-w-sm">
+        <div className='w-full max-w-sm'>
             <div className="flex items-center border-b border-slate-600 py-2">
-                <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder={`${name}`} aria-label="Full name"/>
-                    <button className="flex-shrink-0 bg-slate-500 hover:bg-slate-700 border-slate-500 hover:border-slate-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                <input onKeyDown={handl} ref={value} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder={`${name}`} aria-label="Full name"/>
+                    <button onClick={submit} className="flex-shrink-0 bg-slate-500 hover:bg-slate-700 border-slate-500 hover:border-slate-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
                         Submit
                     </button>
                     <button className="flex-shrink-0 border-transparent border-4 text-slate-600 hover:text-slate-800 text-sm py-1 px-2 rounded" type="button">
                             Cancel
                     </button>
             </div>
-        </form>
+        </div>
     );
 }
 
--- front-end/pages/NewChat.tsx
+++ front-end/pages/NewChat.tsx
@@ -1,6 +1,11 @@
 import Barl from "@/components/Barl";
 import NavBar from "@/components/NavBar";
+import NewContactList from "@/components/NewContactList";
 import RealFooter from "@/components/RealFooter";
+import mhaddaou from '../image/mhaddaou.jpg'
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const NewChat = () =>{
     return (
@@ -10,11 +15,25 @@ const NewChat = () =>{
                 <div className="w-full h-full rounded-2xl flex flex-col  gap-2 ">
                 <NavBar page="Chat"/>
                     <div className="w-full h-[95%] rounded-2xl bg-gray-300 flex gap-1">
-                        <div className="w-full md:w-1/3 rounded-2xl bg-red-400 flex">
+                        <div className="w-full md:w-1/3 rounded-2xl  ">
+                            <NewContactList />
                             
                         </div>
-                        <div className="hidden md:flex w-2/3 rounded-2xl md:flex-col gap-1">
-                            <div className="w-full rounded-2xl bg-red-200 h-[5%]">
+                        <div className="hidden md:flex bg-gradient-to-r from-gray-300 via-slate-400 to-slate-500 w-2/3 rounded-2xl md:flex-col gap-1">
+                            <div className="w-full rounded-2xl  h-[5%] flex items-center justify-around">
+                                <div className="w-1/2 h-full flex items-center pl-8 gap-6">
+                                    <div className="avatar">
+                                        <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                            <Image src={mhaddaou} alt="av" />
+                                        </div>
+                                    </div>
+                                    <div className="font-mono font-extrabold">mhaddaou</div>
+                                </div>
+                                <div className=" w-1/2 h-full flex items-center justify-end pr-8">
+                                    <button>
+                                        <FontAwesomeIcon icon={faBars} flip style={{color: "#f0f2f5",}} />
+                                    </button>
+                                </div>
 
                             </div>
                             <div className="w-full rounded-2xl bg-blue-400 h-[95%]">
