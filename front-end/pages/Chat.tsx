076af51f6f37bc5ab2df33a9b86bf38389d95546
Mohamed Haddaoui <mhaddaou@e2r1p5.1337.ma>
2023-06-06 19:28:18 +0100

    update chat page

--- front-end/pages/Chat.tsx
+++ front-end/pages/Chat.tsx
@@ -8,6 +8,8 @@ import ContactList from '@/components/ContactList';
 import axios from 'axios';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Barl from "@/components/Barl";
+import NavBar from "@/components/NavBar";
 
 export default function Chat() {
   const [id, setId] = useState(0);
@@ -22,18 +24,35 @@ export default function Chat() {
   
     
   return (
-    <div className="bg-slate-800 h-full w-screen">
-        <Header check={true} />
-      <div className="flex flex-row h-screen gap-1 my-2 mx-5 ">
-        <div className="w-1/4  p-4 bg-slate-900 rounded-xl">
-          <ContactList onContactClick={handleContactClick} />
+    <div className="bg-gradient-to-t from-gray-100 to-gray-400 min-h-screen">
+      <div className="container w-full mx-auto h-screen min-h-[1024px] flex flex-row py-2 gap-2">
+        <Barl page="Chat"/>
+        <div className="w-full h-full rounded-2xl flex flex-col  gap-2 ">
+        <NavBar page="Chat"/>
+        <div className="w-full h-[95%] rounded-2xl bg-gray-400 flex gap-1">
+          <div className="w-[25%]  rounded-2xl bg-gray-200">
+            <ContactList onContactClick={handleContactClick} />
+          </div>
+          <div className="w-[75%]  h-full rounded-xl">
+            <ChatHistory chatHistory={chatHistory} id = {id} />
+          </div>
+
+
         </div>
-        <div className="w-1/2 bg-slate-950 p-4 h-full rounded-xl">
-          <ChatHistory chatHistory={chatHistory} id = {id} />
         </div>
-        <div className="w-1/4 bg-slate-900 p-4 rounded-xl">info Field</div>
       </div>
-      <Footer />
     </div>
+
+
   );
 }
+
+{/* <div className="flex flex-row h-screen gap-1 my-2 mx-5 ">
+  <div className="w-1/4  p-4  rounded-xl">
+    <ContactList onContactClick={handleContactClick} />
+  </div>
+  <div className="w-1/2 p-4 h-full rounded-xl">
+    <ChatHistory chatHistory={chatHistory} id = {id} />
+  </div>
+  <div className="w-1/4  p-4 rounded-xl">info Field</div>
+</div> */}
