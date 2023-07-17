1abe0da2d8b7a022e58149b64e14f35ea9dde652
mhaddaou <mhaddaou@student.1337.ma>
2023-07-17 05:55:29 +0100

    up

--- front-end/pages/Anim/Anim.tsx
+++
@@ -1,7 +0,0 @@
-// import '@/styles/anim.css'
-
-export default function Anim() {
-    return (<div style={{ animationName: 'med' ,animationDuration:'5s' , animationIterationCount:'infinite'}} className="  rounded-full
-
-    w-20 h-20 bg-red-500 " ></div>)
-}
